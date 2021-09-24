a1ec0eb9075427500ebf9b156993aec5d373a106
kohei sakurai <ks19ks19ks19@gmail.com>
2021-09-24 15:10:39 +0900

    add comment function, dynamically show comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :blogs
+  resources :blogs do
+    resources :comments
+  end
   resources :sessions, only: %i(new create destroy)
   resources :users, only: %i(new create show)
 end
