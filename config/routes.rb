9736d655a471a5bab6a22dbd795d8450f294fe4a
kohei sakurai <ks19ks19ks19@gmail.com>
2021-09-26 08:54:27 +0900

    add conversations controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :sessions, only: %i(new create destroy)
   resources :users, only: %i(index new create show)
   resources :relationships, only: %i(create destroy)
+  resources :conversations do
+    resources :messages
+  end
 end
